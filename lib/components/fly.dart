ba1d8303b4aa549aa906e1cf21150383702fe6b1
Minsu Jeon <msjeon98@gmail.com>
2020-10-14 22:31:40 +0900

    After chapter 03

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -1,35 +1,67 @@
 import 'dart:ui';
+import 'package:flame/sprite.dart';
 import 'package:langaw/langaw-game.dart';
 
 class Fly {
   final LangawGame game;
+  List<Sprite> flyingSprite;
+  Sprite deadSprite;
+  double flyingSpriteIndex = 0;
   Rect flyRect;
   Paint flyPaint;
   bool isDead = false;
   bool isOffScreen = false;
+  Offset targetLocation;
 
-  Fly(this.game, double x, double y) {
-    flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
-    flyPaint = Paint();
-    flyPaint.color = Color(0xff6ab04c);
+  double get speed => game.tileSize * 3;
+
+  Fly(this.game) {
+    setTargetLocation();
+  }
+
+  void setTargetLocation() {
+    double x = game.rnd.nextDouble() * (game.screenSize.width - (game.tileSize * 1.35));
+    double y = (game.rnd.nextDouble() * (game.screenSize.height - (game.tileSize * 2.85))) + (game.tileSize * 1.5);
+    targetLocation = Offset(x, y);
   }
 
   void render(Canvas c) {
-    c.drawRect(flyRect, flyPaint);
+    if (isDead) {
+      deadSprite.renderRect(c, flyRect.inflate(2));
+    } else {
+      flyingSprite[flyingSpriteIndex.toInt()].renderRect(c, flyRect.inflate(2));
+    }
   }
+
   void update(double t) {
     if (isDead) {
-      print(t);
+      // make the fly fall
       flyRect = flyRect.translate(0, game.tileSize * 12 * t);
       if (flyRect.top > game.screenSize.height) {
         isOffScreen = true;
+      }
+    } else {
+      // flap the wings
+      flyingSpriteIndex += 30 * t;
+      while (flyingSpriteIndex >= 2) {
+        flyingSpriteIndex -= 2;
+      }
+
+      // move the fly
+      double stepDistance = speed * t;
+      Offset toTarget = targetLocation - Offset(flyRect.left, flyRect.top);
+      if (stepDistance < toTarget.distance) {
+        Offset stepToTarget = Offset.fromDirection(toTarget.direction, stepDistance);
+        flyRect = flyRect.shift(stepToTarget);
+      } else {
+        flyRect = flyRect.shift(toTarget);
+        setTargetLocation();
       }
     }
   }
 
   void onTapDown() {
     isDead = true;
-    flyPaint.color = Color(0xffff4757);
     game.spawnFly();
   }
 }
